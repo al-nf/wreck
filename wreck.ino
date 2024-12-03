41ecba391e9433c12c48e9db87375dc37d81d77e
al-nf <alankjfung@gmail.com>
2024-12-03 15:01:11 -0800

    big changes to wreck.ino

--- wreck.ino
+++ wreck.ino
@@ -4,35 +4,24 @@
 #define joystickX A0
 #define joystickY A1
 
-int trackerButton = 3;
 int joystickSW = 4;
-bool track = false;
 int servo1 = 9;
 int servo2 = 10;
-int ledPin = 5;
+int ledPin = 6;
+int sensor = 5;
 
 void setup() {
-    pinMode(trackerButton, INPUT);
-    pinMode(fireButton, INPUT);
     pinMode(joystickX, INPUT);
+    pinMode(joystickY, INPUT);
     pinMode(joystickSW, INPUT);
+    pinMode(sensor, INPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(servo1, OUTPUT);
+    pinMode(servo2, OUTPUT);
 }
 
 void loop() {
-    if (digitalInput(trackerButton) == HIGH)
-    {
-            track = !track;
-    }
-    
-    if (track)
-    {
-        digitalWrite(ledPin, HIGH);
-    }
-    else
-    {
-        digitalWrite(ledPin, LOW);
-    }
-
+    digitalWrite(ledPin, digitalRead(sensor));
     digitalWrite(servo1, analogRead(joystickX));
     digitalWrite(servo2, analogRead(joystickY));
 }
